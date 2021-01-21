afa520eabd7f636498c5ab89f79194e0b0fc6894
ark <67444302+ark-maker-bot@users.noreply.github.com>
2021-01-21 22:33:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# better-tord
+ This api gives you questions for the game truth or dare
---
+++ test.js
@@ -0,0 +1,6 @@
+const r = require('./index');
+const question = r.get_question();
+console.log(question);
+
+const amount = r.amount_of_questions();
+console.log(amount);
